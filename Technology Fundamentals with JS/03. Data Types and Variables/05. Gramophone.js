4d50dd8511c1101040ed3e536ad0b39e70a532b5
Georgi Slaveykov <slaweykow@protonmail.com>
2019-02-07 21:50:45 +0100

    📚 Pushing the lab [ Data Types & Variables ]

---
+++ Technology Fundamentals with JS/03. Data Types and Variables/05. Gramophone.js
@@ -0,0 +1,8 @@
+function calculateRotations(bandName, albumName, songName) {
+  let songDuration = (albumName.length * bandName.length) * songName.length / 2;
+  let rotations = Math.ceil(songDuration / 2.5);
+
+  console.log(`The plate was rotated ${rotations} times.`);
+}
+
+// calculateRotations('Black Sabbath', 'Paranoid', 'War Pigs');
