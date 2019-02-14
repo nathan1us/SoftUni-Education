72109fc54c97a06e665d04639bddd15e0a29db7e
Georgi Slaveykov <slaweykow@protonmail.com>
2019-02-14 20:53:20 +0200

    Revert "📚 Pushing the lab [ Data Types & Variables ]"
    This reverts commit bd8a474cadd79348eb9fe5a21b4432107d59801e.
    
    Revert

--- Technology Fundamentals with JS/03. Data Types and Variables/Lab/01. Concatenate Names.js
+++
@@ -1,9 +0,0 @@
-function concatenate(firstName, lastName, del) {
-  console.log(`${firstName}${del}${lastName}`);
-}
-
-/*
-concatenate('John', 'Smith', '->');
-concatenate('Jan', 'White', '<->');
-concatenate('Linda', 'Terry', '=>');
-*/
--- Technology Fundamentals with JS/03. Data Types and Variables/Lab/05. Gramophone.js
+++
@@ -1,8 +0,0 @@
-function calculateRotations(bandName, albumName, songName) {
-  let songDuration = (albumName.length * bandName.length) * songName.length / 2;
-  let rotations = Math.ceil(songDuration / 2.5);
-
-  console.log(`The plate was rotated ${rotations} times.`);
-}
-
-// calculateRotations('Black Sabbath', 'Paranoid', 'War Pigs');
